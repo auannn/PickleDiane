b5a944c5fff54e585babf6321abd192783f6e364
auannn <weowast@gmail.com>
2026-08-26 20:03:35 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,32 @@
+import telebot
+import random
+import os
+
+TOKEN = os.getenv("TOKEN")
+
+# Создайте бота
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands = ["start"])
+def start_cmd(message):
+    bot.send_message(message.chat.id,"Привет я твой бот!")
+
+@bot.message_handler(commands = ["info"])
+def start_cmd(message):
+    bot.send_message(message.chat.id,"Бот создан @vloov_q")
+
+@bot.message_handler(commands = ["funfact"])
+def start_cmd(message):
+    facts = [
+        "На самом деле Даен не огурец, а морковь",
+        "Рик с-137 искал Рика Прайма 24 лет",
+        "Морти считается аутистом"
+    ]
+    fact = random.choice(facts)
+    bot.send_message(message.chat.id, "Интересный факт:\n " + fact)
+
+
+
+
+print("Бот работает ...")
+bot.infinity_polling() # Запуск бота
